18afbefd9f404018a8757156f4699f3fa41f1a23
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-07-10 19:25:47 +0200

    fine esercizio

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $menu = [
+        "Home",
+        "Photos",
+        "Places",
+    ];
+
+    return view('welcome', compact('menu'));
 });
